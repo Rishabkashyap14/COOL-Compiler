989e16938884905eeb6416e7925046b2c6fae02b
shreyavenugopal <shrey1010.svg@gmail.com>
2021-02-28 23:32:19 +0530

    The bad header file

---
+++ sample-codes/lexical/cool.h
@@ -0,0 +1,28 @@
+
+#define CLASS 1
+#define ELSE 2
+#define FI 22
+#define IF 4
+#define IN 5
+#define INHERITS 6
+#define LET 7
+#define LOOP 8
+#define POOL 9
+#define THEN 10
+#define WHILE 11
+#define CASE 12
+#define ESAC 13
+#define OF 14
+#define NEW 15
+#define ISVOID 16
+#define NOT 17
+#define TRUE 18
+#define FALSE 19
+#define TYPE_ID 20
+#define OBJ_ID 21
+#define DARROW 23
+#define ASSIGN 24
+#define LE 25
+#define ERROR 26
+#define LET_STMT
+
